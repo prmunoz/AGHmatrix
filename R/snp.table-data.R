1c64f06d6d04972ca3e2eb9a855ca84280b0b347
rramadeu <rramadeu@gmail.com>
2016-03-31 21:30:57 -0300

    example molecular data

---
+++ R/snp.table-data.R
@@ -0,0 +1,19 @@
+#' Molecular Data
+#'
+#' Dummy data for construction of relationship matrix based on SNPs. Matrix (markers x genotype). Dosage as 0,1, and 2, and missing value as -9.
+#'
+#' @docType data
+#'
+#' @usage data(snp.table)
+#'
+#' @format table
+#'
+#' @keywords datasets
+#'
+#'
+#' @references 
+#'
+#' @examples data(snp.table)
+#'
+#' @name snp.table
+NULL
