a28b6b1759ed876e5df7584089ea58ab9ae55422
Rodrigo Rampazo Amadeu <rramadeu@gmail.com>
2016-09-29 22:01:01 -0300

    Correction Fixed (Pmatrix)

--- R/Gmatrix.R
+++ R/Gmatrix.R
@@ -5,7 +5,7 @@
 # File: Gmatrix.R 							#
 # Contains: Gmatrix 							#
 # 									#
-# Written by Rodrigo Rampazo Amadeu 					#
+# Written by Rodrigo Rampazo Amadeu and Marcio Resende Jr.	#
 # 									#
 # First version: Feb-2014 						#
 # Last update: 31-Mar-2016 						#
@@ -32,124 +32,82 @@
 #' #Build Gmatrix
 #' Gmatrix(snp.table,missingValue=-9)
 #'
-#' @author Rodrigo R Amadeu, \email{rramadeu@@gmail.com}
+#' @author Rodrigo R Amadeu, \email{rramadeu@@gmail.com} and Marcio Resende Jr.
 #'
 #' @references \emph{Van Raden, P. M. (2008). Efficient methods to compute genomic predictions. Journal of Dairy Science, 91(11), 4414-4423.}
 #' @references \emph{Powell, J. E. (2010). Reconciling the analysis of IBD and IBS in complex trait studies. Nature Reviews Genetics, 11(11), 800-805.}
 #'
 #' @export
 
-Gmatrix = function( SNPdata = NULL,
-                    method = 2,
-                    missingValue = NA,
-                    maf = 0,
-                    verify.posdef=TRUE){
-
+Gmatrix <- function (SNPdata = NULL, method = 2, missingValue = NA, maf = 0, verify.posdef = TRUE) 
+{
     Time = proc.time()
-
-    if(is.null(SNPdata)){
+    if (is.null(SNPdata)) {
         stop(deparse("Please define the variable SNPdata"))
     }
-    if (missing(missingValue)){ stop(deparse("missingValue not defined"))}
-    
-    if(method==1)
-        method="Powell"
-    if(method==2)
-        method="VanRaden"
-    if(method==3)
-        method="MarkersMatrix"
-
-    if (all(method != c("Powell","VanRaden","MarkersMatrix"))){ stop("Method to build Gmatrix has to be either (1) Powell or (2) VanRaden or (3) for the Amount of Markers Between Individuals  Matrix") }
-
-    SNPmatrix = as.matrix(SNPdata)
-
-    if (!is.na(missingValue)){
-        m <- match(SNPmatrix, missingValue ,0)
+    if (missing(missingValue)) {
+        stop(deparse("missingValue not defined"))
+    }
+    if (method == 1) 
+        method = "Powell"
+    if (method == 2) 
+        method = "VanRaden"
+    if (method == 3) 
+        method = "MarkersMatrix"
+    if (all(method != c("Powell", "VanRaden", "MarkersMatrix"))) {
+        stop("Method to build Gmatrix has to be either (1) Powell or (2) VanRaden or (3) for the Amount of Markers Between Individuals  Matrix")
+    }
+ SNPmatrix = t(matrix(as.numeric(SNPdata),ncol = ncol(SNPdata),dimnames = list(c(rownames(SNPdata)),c(colnames(SNPdata)))))
+    if (!is.na(missingValue)) {
+        m <- match(SNPmatrix, missingValue, 0)
         SNPmatrix[m > 0] <- NA
     }
-
     NumberMarkers = nrow(SNPmatrix)
     nindTotal = rowSums(!is.na(SNPmatrix))
     nindAbs = max(nindTotal)
-
-    cat("	Number of Individuals:", max(nindTotal),"\n\n")
-    cat("	Number of Markers:", NumberMarkers ,"\n")
-
-    alelleFreq = rowSums(SNPmatrix,na.rm=TRUE)/(2*nindTotal)
-
-    ##############################################
-    #Swap major with minor alelle (0,1,2 to 2,1,0)
-    major.minor <- which(alelleFreq < 0.5)
-    if( length(major.minor>0) )
-        SNPmatrix[major.minor,] <- 1+ (SNPmatrix[major.minor,] - 1)*-1
-    ##############################################
-   
-    if( maf == 0){
-        exclude <- c(which(alelleFreq==0))
-
-        if(length(exclude)>0){
-            SNPmatrix <- SNPmatrix[-exclude,]
-            alelleFreq <- alelleFreq[-exclude]
-        }
-    }
+    cat("\tNumber of Individuals:", max(nindTotal), "\n\n")
+    cat("\tNumber of Markers:", NumberMarkers, "\n")
+    alelleFreq = function(x,y){(2*length(which(x==y))+length(which(x==1)))/(2*length(which(!is.na(x))))}
     
-    if( maf > 0){
-        exclude <- c(which(alelleFreq<maf),which(alelleFreq>(1-maf)))
-        if(length(exclude)>0){
-            SNPmatrix <- SNPmatrix[-exclude,]
-            alelleFreq = alelleFreq[-exclude]
-            NumberMarkers = nrow(SNPmatrix)
-        }
-        cat("	Number of Markers after maf:", NumberMarkers ,"\n")
-    }
-
-    if(method=="MarkersMatrix"){ #calculate the number of markers for each ind pair
+    Frequency = cbind(apply(SNPmatrix,1,function(x) alelleFreq(x,0)),apply(SNPmatrix,1,function(x) alelleFreq(x,2)))
+    if (any(Frequency[,1]<= maf) & maf != 0){
+        SNPmatrix = SNPmatrix[-which(Frequency[,1]<= maf),]
+        Frequency = as.matrix(Frequency[-which(Frequency[,1]<= maf),])
+        NumberMarkers = nrow(SNPmatrix)
+    }	
+    Sum2pq = 2*t(Frequency[,1])%*%Frequency[,2]
+    FreqP = matrix(rep(Frequency[,2],each = ncol(SNPmatrix)),ncol = nrow(SNPmatrix))
+    if (method == "MarkersMatrix") {
         Gmatrix <- !is.na(SNPmatrix)
-        Gmatrix <- crossprod(Gmatrix,Gmatrix)
+        Gmatrix <- crossprod(Gmatrix, Gmatrix)
         return(Gmatrix)
     }
-
-    NumberMarkers = nrow(SNPmatrix)
-
-    Frequency = cbind((1-alelleFreq),alelleFreq)
-    FreqP<-alelleFreq
-
-#    Pmatrix <- matrix(rep(alelleFreq,max(nindTotal)),nrow=max(nindTotal),byrow=TRUE)
-    Pmatrix <- matrix(rep(alelleFreq,max(nindTotal)),ncol=max(nindTotal))
-    
-
-    if( method=="VanRaden"){
-        Zmatrix <- SNPmatrix-2*Pmatrix
-        Zmatrix[is.na(Zmatrix)] = 0 #Missing Values
-        Gmatrix <- crossprod(Zmatrix,Zmatrix)
-        sumpi <- sum(alelleFreq*(1-alelleFreq))
-        Gmatrix <- Gmatrix/(2*sumpi)
+    if (method == "VanRaden") {
+	SNP_VanRaden = as.matrix(t(SNPmatrix))
+        SNP_VanRaden = SNP_VanRaden - 2*FreqP
+        SNP_VanRaden[is.na(SNP_VanRaden)] <- 0
+        Gmatrix = (crossprod(t(SNP_VanRaden),t(SNP_VanRaden)))/as.numeric(Sum2pq)
     }
-
-    if( method == "Powell"){
-        SNP.Visscher <- SNPmatrix
-        FreqPQ = matrix(rep(2*Frequency[,1] * Frequency[,2],nindAbs),ncol=nindAbs)
-        G.all = (SNP.Visscher^2 - (1+2*Pmatrix)*SNP.Visscher + 2*Pmatrix^2)/FreqPQ
-        SNP.Visscher= (SNP.Visscher-(2*Pmatrix))/sqrt(FreqPQ)
-        SNP.Visscher[is.na(SNP.Visscher)] <- 0
-        Gmatrix = (crossprod(SNP.Visscher,SNP.Visscher))/max(NumberMarkers)
-      
-        G.ii = as.matrix(colSums(G.all,na.rm = T))
-        G.ii.hat = 1+(G.ii)/max(NumberMarkers)
-        diag(Gmatrix) = G.ii.hat
+    if (method == "Powell") {
+        
+		SNP_Visscher = as.matrix(t(SNPmatrix))		
+		FreqPQ = matrix(rep(2*Frequency[,1] * Frequency[,2],each = ncol(SNPmatrix)),ncol = nrow(SNPmatrix))			
+		G_all = (SNP_Visscher^2 - (1+2*FreqP)*SNP_Visscher + 2*FreqP^2)/FreqPQ
+		G_ii = as.matrix(colSums(t(G_all),na.rm = T))
+		SNP_Visscher= (SNP_Visscher-(2*FreqP))/sqrt(FreqPQ)
+		G_ii_hat = 1+(G_ii)/NumberMarkers
+		SNP_Visscher[is.na(SNP_Visscher)] <- 0
+		Gmatrix = (crossprod(t(SNP_Visscher),t(SNP_Visscher)))/NumberMarkers
+		diag(Gmatrix) = G_ii_hat
     }
-    
-   ##############################################
-   #Verifying if it is positive definite
-    if( verify.posdef ){
-        e.values<-eigen(Gmatrix,symmetric=TRUE)$values
-        indicator<-length(which(e.values <= 0))
-        if( indicator > 0)
-            cat("Matrix is NOT posiive definite. It has ", indicator," eigenvalues <= 0")
+    if (verify.posdef) {
+        e.values <- eigen(Gmatrix, symmetric = TRUE)$values
+        indicator <- length(which(e.values <= 0))
+        if (indicator > 0) 
+            cat("Matrix is NOT posiive definite. It has ", indicator, 
+                " eigenvalues <= 0")
     }
-     ##############################################
-
-    Time = as.matrix(proc.time()-Time)
-    cat("Completed! Time =", Time[3]," seconds \n")
+    Time = as.matrix(proc.time() - Time)
+    cat("Completed! Time =", Time[3], " seconds \n")
     return(Gmatrix)
 }
