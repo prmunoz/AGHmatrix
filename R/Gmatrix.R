74e8f2e42cb71c240e1363aa2c9567b109b56f6f
rramadeu <rramadeu@gmail.com>
2016-10-06 22:23:38 -0300

    Faster Gmatrix function and updated manual

--- R/Gmatrix.R
+++ R/Gmatrix.R
@@ -5,32 +5,32 @@
 # File: Gmatrix.R 							#
 # Contains: Gmatrix 							#
 # 									#
-# Written by Rodrigo Rampazo Amadeu and Marcio Resende Jr.	#
+# Written by Rodrigo Rampazo Amadeu and Marcio Resende Jr.		#
 # 									#
 # First version: Feb-2014 						#
-# Last update: 31-Mar-2016 						#
+# Last update: 06-Oct-2016 						#
 # License: GNU General Public License version 2 (June, 1991) or later 	#
 # 									#
 #########################################################################
 
 #' Construction of Relationship Matrix G
 #'
-#' Given a matrix (markers x individual) choosing a method ((1) Powell or (2) VanRaden), a missing value, and a maf threshold, return a G matrix. Also the function can calculate the number of markers shared two-by-two individuals (method 3).
+#' Given a matrix (individual x markers), a method ((1) Powell or (2) VanRaden), a missing value, and a maf threshold, return a G matrix. Also the function can calculate the number of markers shared two-by-two individuals (method 3).
 #'
-#' @param SNPdata matrix (n x m), where n is marker information (coded as 0,1,2,NA) and m is individual names (a string). 
+#' @param SNPdata matrix (n x m), where n is is individual names and m is marker names (coded inside the matrix as 0,1,2,missingValue). 
 #' @param method "Powell" (1), "VanRaden" (2), shared markers between individual (3). Default=2.
-#' @param missingValue missing value in data. Default=NA.
+#' @param missingValue missing value in data. Default=-9.
 #' @param maf max of missing data accepted to each marker. Default=0.
 #' @param verify.posdef verify if the resulting matrix is positive-definite. Default=TRUE.
 #'
 #' @return Matrix with the Relationship between the individuals
 #'
 #' @examples
-#' data(snp.table)
+#' data(snp.pine)
 #' #Verifying if data is coded as 0,1,2 and missing value.
-#' snp.table
+#' str(snp.pine)
 #' #Build Gmatrix
-#' Gmatrix(snp.table,missingValue=-9)
+#' PineGmatrix <- Gmatrix(snp.pine,missingValue=-9,maf=0.05)
 #'
 #' @author Rodrigo R Amadeu, \email{rramadeu@@gmail.com} and Marcio Resende Jr.
 #'
@@ -39,7 +39,7 @@
 #'
 #' @export
 
-Gmatrix <- function (SNPdata = NULL, method = 2, missingValue = NA, maf = 0, verify.posdef = TRUE) 
+Gmatrix <- function (SNPdata = NULL, method = 2, missingValue = -9, maf = 0, verify.posdef = TRUE) 
 {
     Time = proc.time()
     if (is.null(SNPdata)) {
@@ -57,55 +57,65 @@ Gmatrix <- function (SNPdata = NULL, method = 2, missingValue = NA, maf = 0, ver
     if (all(method != c("Powell", "VanRaden", "MarkersMatrix"))) {
         stop("Method to build Gmatrix has to be either (1) Powell or (2) VanRaden or (3) for the Amount of Markers Between Individuals  Matrix")
     }
- SNPmatrix = t(matrix(as.numeric(SNPdata),ncol = ncol(SNPdata),dimnames = list(c(rownames(SNPdata)),c(colnames(SNPdata)))))
+    if(class(SNPmatrix)!="matrix"){
+        cat("SNPmatrix class is:",class(SNPmatrix),"\n")
+        stop("SNPmatrix class must be matrix. Please verify it.")
+        }
     if (!is.na(missingValue)) {
         m <- match(SNPmatrix, missingValue, 0)
         SNPmatrix[m > 0] <- NA
     }
-    NumberMarkers = nrow(SNPmatrix)
-    nindTotal = rowSums(!is.na(SNPmatrix))
+    NumberMarkers = ncol(SNPmatrix)
+    nindTotal = colSums(!is.na(SNPmatrix))
     nindAbs = max(nindTotal)
-    cat("\tNumber of Individuals:", max(nindTotal), "\n\n")
+    cat("\tNumber of Individuals:", max(nindTotal), "\n")
     cat("\tNumber of Markers:", NumberMarkers, "\n")
-    alelleFreq = function(x,y){(2*length(which(x==y))+length(which(x==1)))/(2*length(which(!is.na(x))))}
-    
-    Frequency = cbind(apply(SNPmatrix,1,function(x) alelleFreq(x,0)),apply(SNPmatrix,1,function(x) alelleFreq(x,2)))
-    if (any(Frequency[,1]<= maf) & maf != 0){
-        SNPmatrix = SNPmatrix[-which(Frequency[,1]<= maf),]
-        Frequency = as.matrix(Frequency[-which(Frequency[,1]<= maf),])
-        NumberMarkers = nrow(SNPmatrix)
-    }	
-    Sum2pq = 2*t(Frequency[,1])%*%Frequency[,2]
-    FreqP = matrix(rep(Frequency[,2],each = ncol(SNPmatrix)),ncol = nrow(SNPmatrix))
+    alelleFreq = function(x, y) {
+        (2 * length(which(x == y)) + length(which(x == 1)))/(2 * 
+            length(which(!is.na(x))))
+    }
+    Frequency = cbind(apply(SNPmatrix, 2, function(x) alelleFreq(x, 
+        0)), apply(SNPmatrix, 2, function(x) alelleFreq(x, 2)))
+    if (any(Frequency[, 1] <= maf) & maf != 0) {
+        cat("\t", length(which(Frequency[, 1] <= maf)), "markers dropped due to maf cutoff of", maf, "\n")
+        SNPmatrix = SNPmatrix[,-which(Frequency[, 1] <= maf)]
+        cat("\t", ncol(SNPmatrix), "markers kept \n")
+        Frequency = as.matrix(Frequency[-which(Frequency[,1] <= 
+            maf), ])
+        NumberMarkers = ncol(SNPmatrix)
+    }
+    Sum2pq = 2 * t(Frequency[, 1]) %*% Frequency[, 2]
+    FreqP = matrix(rep(Frequency[, 2], each = nrow(SNPmatrix)), 
+        ncol = ncol(SNPmatrix))
     if (method == "MarkersMatrix") {
         Gmatrix <- !is.na(SNPmatrix)
-        Gmatrix <- crossprod(Gmatrix, Gmatrix)
+        Gmatrix <- tcrossprod(Gmatrix, Gmatrix)
         return(Gmatrix)
     }
     if (method == "VanRaden") {
-	SNP_VanRaden = as.matrix(t(SNPmatrix))
-        SNP_VanRaden = SNP_VanRaden - 2*FreqP
-        SNP_VanRaden[is.na(SNP_VanRaden)] <- 0
-        Gmatrix = (crossprod(t(SNP_VanRaden),t(SNP_VanRaden)))/as.numeric(Sum2pq)
+       # SNP.VanRaden = SNPmatrix
+        SNPmatrix = SNPmatrix- 2 * FreqP
+        SNPmatrix[is.na(SNPmatrix)] <- 0
+        Gmatrix = (tcrossprod(SNPmatrix, SNPmatrix))/as.numeric(Sum2pq)
     }
     if (method == "Powell") {
-        
-		SNP_Visscher = as.matrix(t(SNPmatrix))		
-		FreqPQ = matrix(rep(2*Frequency[,1] * Frequency[,2],each = ncol(SNPmatrix)),ncol = nrow(SNPmatrix))			
-		G_all = (SNP_Visscher^2 - (1+2*FreqP)*SNP_Visscher + 2*FreqP^2)/FreqPQ
-		G_ii = as.matrix(colSums(t(G_all),na.rm = T))
-		SNP_Visscher= (SNP_Visscher-(2*FreqP))/sqrt(FreqPQ)
-		G_ii_hat = 1+(G_ii)/NumberMarkers
-		SNP_Visscher[is.na(SNP_Visscher)] <- 0
-		Gmatrix = (crossprod(t(SNP_Visscher),t(SNP_Visscher)))/NumberMarkers
-		diag(Gmatrix) = G_ii_hat
+        FreqPQ = matrix(rep(2 * Frequency[, 1] * Frequency[, 
+            2], each = nrow(SNPmatrix)), ncol = ncol(SNPmatrix))
+        G.all = (SNPmatrix^2 - (1 + 2 * FreqP) * SNPmatrix + 
+            2 * FreqP^2)/FreqPQ
+        G.ii = as.matrix(colSums(t(G.all), na.rm = T))
+        SNPmatrix = (SNPmatrix - (2 * FreqP))/sqrt(FreqPQ)
+        G.ii.hat = 1 + (G.ii)/NumberMarkers
+        SNPmatrix[is.na(SNPmatrix)] <- 0
+        Gmatrix = (tcrossprod(SNPmatrix, SNPmatrix))/NumberMarkers
+        diag(Gmatrix) = G.ii.hat
     }
     if (verify.posdef) {
         e.values <- eigen(Gmatrix, symmetric = TRUE)$values
         indicator <- length(which(e.values <= 0))
         if (indicator > 0) 
-            cat("Matrix is NOT posiive definite. It has ", indicator, 
-                " eigenvalues <= 0")
+            cat("\t Matrix is NOT posiive definite. It has ", indicator, 
+                " eigenvalues <= 0 \n \n")
     }
     Time = as.matrix(proc.time() - Time)
     cat("Completed! Time =", Time[3], " seconds \n")
---
+++ R/snp.pine-data.R
@@ -0,0 +1,19 @@
+#' Molecular Data
+#'
+#' SNP marker matrix from Pinus coded as 0,1, and 2, and missing value as -9. Supplementary data from Resende (2012).
+#'
+#' @docType data
+#'
+#' @usage data(snp.pine)
+#'
+#' @format matrix
+#'
+#' @keywords datasets
+#'
+#'
+#' @references Resende, Márcio FR, Patricio Muñoz, Marcos DV Resende, Dorian J. Garrick, Rohan L. Fernando, John M. Davis, Eric J. Jokela, Timothy A. Martin, Gary F. Peter, and Matias Kirst. "Accuracy of genomic selection methods in a standard data set of loblolly pine (Pinus taeda L.)." Genetics 190, no. 4 (2012): 1503-1510.
+#'
+#' @examples data(snp.pine)
+#'
+#' @name snp.pine
+NULL
