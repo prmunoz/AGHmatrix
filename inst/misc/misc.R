19c81ef870d15482cf105f61bfe25308913b0439
rramadeu <rramadeu@gmail.com>
2018-05-11 11:44:15 -0400

    update tutorial w/ Endelman2018 data

--- inst/misc/misc.R
+++ inst/misc/misc.R
@@ -43,6 +43,48 @@ Gmatrix(M1,ploidy=4)
 Gmatrix(M2,ploidy=4)
 
 ## Including Data from Endelman 2018
+MolPotato <- read.table("~/git/AGHmatrix/data/FileS2.csv",header=TRUE,sep=",")
+ind <- MolPotato[[1]]
+MolPotato <- as.matrix(MolPotato[,-1])
+rownames(MolPotato) <- ind
+snp.potato <- MolPotato
+
+save(snp.potato,file="snp.potato.rdata")
+
+library(AGHmatrix)
+G1 <- Gmatrix(MolPotato,ploidy=4)
+G2 <- Gmatrix(MolPotato,ploidy=4,method="Slater")
+G3 <- Gmatrix(MolPotato,ploidy=4,method="Endelman")
+
+PedPotato <- read.table("~/git/AGHmatrix/data/FileS3.csv",header=TRUE,sep=",")
+PedPotato[,2] <- PedPotato[PedPotato[,2],1]
+PedPotato[which(PedPotato[,3]==0),3] <- NA
+PedPotato[which(PedPotato[,4]==0),4] <- NA
+PedPotato[,3] <- PedPotato[PedPotato[,3],1]
+PedPotato[,4] <- PedPotato[PedPotato[,4],1]
+
+ped.potato <- PedPotato[,c(2,3,4)]
+ped.potato$GID <- as.character(ped.potato$GID)
+ped.potato$Mother <- as.character(ped.potato$Mother)
+ped.potato$Father <- as.character(ped.potato$Father)
+
+ped.potato[which(is.na(ped.potato[,3])),3] <- "0"
+ped.potato[which(is.na(ped.potato[,2])),2] <- "0"
+
+A1<-Amatrix(ped.potato)
+names(ped.potato)[1]<-"Ind"
+A1<-Amatrix(ped.potato)
+save(ped.potato,file="ped.potato.rdata")
+
+data("snp.potato")
+?Gmatrix
+
+Gmatrix.VanRaden <- Gmatrix(snp.potato, method="VanRaden", ploidy=4)
+Gmatrix.Endelman <- Gmatrix(snp.potato, method="Endelman", ploidy=4) 
+Gmatrix.Slater <- Gmatrix(snp.potato, method="Slater", ploidy=4)
+Gmatrix.Pseudodiploid <- Gmatrix(snp.potato, method="VanRaden", ploidy=4, pseudo.diploid=TRUE) 
+
+Amatrix.potato <- Amatrix(ped.potato, ploidy=4)
 
 
 
